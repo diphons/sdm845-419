afa6bd954f5fed1f6d09538e2968fd5862a496f9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-11-01 13:24:35 +0530

    techpack: display: Import xiaomi modification from alioth-r-oss
    Change-Id: I08a3c81e1471fb9b3158c45a57c90dcf7ab3e705
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/msm_drv.h
+++ techpack/display/msm/msm_drv.h
@@ -74,6 +74,9 @@ struct msm_gem_vma;
 
 #define TEARDOWN_DEADLOCK_RETRY_MAX 5
 
+extern atomic_t resume_pending;
+extern wait_queue_head_t resume_wait_q;
+
 struct msm_file_private {
 	rwlock_t queuelock;
 	struct list_head submitqueues;
@@ -129,6 +132,7 @@ enum msm_mdp_plane_property {
 	PLANE_PROP_SRC_CONFIG,
 	PLANE_PROP_FB_TRANSLATION_MODE,
 	PLANE_PROP_MULTIRECT_MODE,
+	PLANE_PROP_MI_LAYER_INFO,
 
 	/* total # of properties */
 	PLANE_PROP_COUNT
@@ -165,6 +169,7 @@ enum msm_mdp_crtc_property {
 	CRTC_PROP_CAPTURE_OUTPUT,
 
 	CRTC_PROP_IDLE_PC_STATE,
+	CRCT_PROP_MI_FOD_SYNC_INFO,
 
 	/* total # of properties */
 	CRTC_PROP_COUNT
@@ -713,6 +718,8 @@ struct msm_drm_private {
 	bool shutdown_in_progress;
 
 	struct msm_idle idle;
+
+	ktime_t  complete_commit_time;
 
 	struct pm_qos_request pm_irq_req;
 	struct delayed_work pm_unreq_dwork;
