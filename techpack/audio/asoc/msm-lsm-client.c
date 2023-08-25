b23322f8ee00d8e68626ae687bd235c134b1bdb6
Michael Bestas <mkbestas@lineageos.org>
2023-08-25 12:02:12 +0300

    Merge tag 'LA.UM.9.12.r1-15600-SMxx50.0' of https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel into main
    "LA.UM.9.12.r1-15600-SMxx50.0"
    
    * tag 'LA.UM.9.12.r1-15600-SMxx50.0' of https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel:
      ASoC: msm-pcm-host-voice: Check validity of session idx
      dsp: q6lsm: Address use after free for mmap handle
      dsp: q6lsm: Add check for payload buffer
      ASoC: Resolve use after free in listen sound client
      ASoC: dsp: q6core: Avoid use after free
      ASoC: msm-pcm-q6-v2: Add dsp buf check
      ASoC: dsp: q6core: Avoid use after free
      ASoC: msm-pcm-host-voice: Address buffer overflow in hpcm copy
      ASoC: msm-pcm-q6-v2: Add dsp buf check
      dsp: q6core: Avoid OOB access in q6core
      ASoC: msm-pcm-host-voice: Handle OOB access in hpcm_start
      dsp: afe: Add check for num_channels
      dsp: asm: validate payload size before access
      dsp: afe: Add check for sidetone iir config copy size
      dsp: q6voice: Add buf size check for cvs cal data
      ASoC: msm-pcm-voip: Avoid interger underflow
      dsp: afe: check for param size before copying
      dsp: q6core: validate payload size before access for AVCS
    
    Change-Id: I586bec33dde9200927e0d6226237aa932a5f7531

--- techpack/audio/asoc/msm-lsm-client.c
+++ techpack/audio/asoc/msm-lsm-client.c
@@ -2163,6 +2163,7 @@ static int msm_lsm_ioctl_compat(struct snd_pcm_substream *substream,
 	case SNDRV_LSM_GET_MODULE_PARAMS_32: {
 		struct lsm_params_get_info_32 p_info_32, *param_info_rsp = NULL;
 		struct lsm_params_get_info *p_info = NULL;
+		prtd->lsm_client->get_param_payload = NULL;
 
 		memset(&p_info_32, 0 , sizeof(p_info_32));
 		if (!prtd->lsm_client->use_topology) {
@@ -2213,6 +2214,7 @@ static int msm_lsm_ioctl_compat(struct snd_pcm_substream *substream,
 				__func__, err);
 			kfree(p_info);
 			kfree(prtd->lsm_client->get_param_payload);
+			prtd->lsm_client->get_param_payload = NULL;
 			goto done;
 		}
 
@@ -2223,6 +2225,7 @@ static int msm_lsm_ioctl_compat(struct snd_pcm_substream *substream,
 			err = -ENOMEM;
 			kfree(p_info);
 			kfree(prtd->lsm_client->get_param_payload);
+			prtd->lsm_client->get_param_payload = NULL;
 			goto done;
 		}
 
@@ -2247,6 +2250,7 @@ free:
 		kfree(p_info);
 		kfree(param_info_rsp);
 		kfree(prtd->lsm_client->get_param_payload);
+		prtd->lsm_client->get_param_payload = NULL;
 		break;
 	}
 	case SNDRV_LSM_REG_SND_MODEL_V2:
@@ -2458,6 +2462,7 @@ static int msm_lsm_ioctl(struct snd_pcm_substream *substream,
 
 	case SNDRV_LSM_GET_MODULE_PARAMS: {
 		struct lsm_params_get_info temp_p_info, *p_info = NULL;
+		prtd->lsm_client->get_param_payload = NULL;
 
 		memset(&temp_p_info, 0, sizeof(temp_p_info));
 		if (!prtd->lsm_client->use_topology) {
@@ -2529,6 +2534,7 @@ static int msm_lsm_ioctl(struct snd_pcm_substream *substream,
 free:
 		kfree(p_info);
 		kfree(prtd->lsm_client->get_param_payload);
+		prtd->lsm_client->get_param_payload = NULL;
 		break;
 	}
 	case SNDRV_LSM_EVENT_STATUS:
