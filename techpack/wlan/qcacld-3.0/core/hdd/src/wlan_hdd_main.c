bb076319c3885ab6fb854df7ef548f855ee6003c
Rudi Setiyawan <diphons@gmail.com>
2023-12-29 16:29:57 +0700

    qcacld: wlan_hdd_main: don't stop exiting debugfs for non sdm845 Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- techpack/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ techpack/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -16070,10 +16070,16 @@ static QDF_STATUS hdd_qdf_init(void)
 
 	status = hdd_qdf_print_init();
 	if (QDF_IS_STATUS_ERROR(status))
+#ifndef CONFIG_ARCH_SDM845
+		goto exit;
+#endif
 
 	status = qdf_debugfs_init();
 	if (QDF_IS_STATUS_ERROR(status)) {
 		hdd_err("Failed to init debugfs; status:%u", status);
+#ifndef CONFIG_ARCH_SDM845
+		goto print_deinit;
+#endif
 	}
 
 	qdf_lock_stats_init();
