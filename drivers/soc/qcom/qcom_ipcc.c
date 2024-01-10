bittencourtjulival/eclipse_kernel_xiaomi_stone

qcom: qcom_ipcc: request ipcc irq IRQF_NO_SUSPEND
Request ipcc irq with irqf no suspend flag.
This is required as to handle IPCC interrupt,
during suspend phase.

Change-Id: I5c34c5d446bd2cc2187550455071f35637a2c4b3
Signed-off-by: Mayank Grover <groverm@codeaurora.org>

drivers/soc/qcom/qcom_ipcc.c
@@ -390,13 +390,12 @@ static int qcom_ipcc_probe(struct platform_device *pdev)
 	}
 
 	ret = devm_request_irq(&pdev->dev, proto_data->irq, qcom_ipcc_irq_fn,
-				IRQF_TRIGGER_HIGH, name, proto_data);
+				IRQF_TRIGGER_HIGH | IRQF_NO_SUSPEND, name, proto_data);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "Failed to register the irq: %d\n", ret);
 		goto err_req_irq;
 	}
 
-	enable_irq_wake(proto_data->irq);
 	platform_set_drvdata(pdev, proto_data);
 
 	return 0;

diphons/sdm845-419

drivers/soc/qcom/qcom_ipcc.c
@@ -400,13 +400,12 @@ static int qcom_ipcc_probe(struct platform_device *pdev)
 	}
 
 	ret = devm_request_irq(&pdev->dev, proto_data->irq, qcom_ipcc_irq_fn,
-				IRQF_TRIGGER_HIGH, name, proto_data);
+				IRQF_NO_SUSPEND | IRQF_TRIGGER_HIGH, name, proto_data);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "Failed to register the irq: %d\n", ret);
 		goto err_req_irq;
 	}
 
-	enable_irq_wake(proto_data->irq);
 	platform_set_drvdata(pdev, proto_data);
 	register_syscore_ops(&msm_ipcc_pm_ops);
 