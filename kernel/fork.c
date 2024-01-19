1cef5901092b4f9a641c77d88fe5e2a741094f67
Rudi Setiyawan <diphons@gmail.com>
2024-01-19 16:44:23 +0700

    kernel: Fixup miss merge Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -94,6 +94,7 @@
 #include <linux/livepatch.h>
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
+#include <linux/devfreq_boost.h>
 #include <linux/scs.h>
 #include <linux/cpuset.h>
 
@@ -2372,7 +2373,7 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	if (task_is_zygote(current)) {
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
 	}
 
 	/*
