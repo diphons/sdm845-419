296e254616a73d2b5c2a331d07fdc81fbbcef918
Rudi Setiyawan <diphons@gmail.com>
2024-03-04 23:17:45 +0700

    Revert "thermal: clear throttling when temperature < clear point"
    This reverts commit 4d21ac4af00676445eb7ba0db373ed50d68a2841.

--- drivers/thermal/step_wise.c
+++ drivers/thermal/step_wise.c
@@ -54,9 +54,10 @@ static unsigned long get_target_state(struct thermal_instance *instance,
 	unsigned long next_target;
 
 	/*
-	 * If the throttle condition is not reached, clear the throttling.
+	 * If the throttle condition is not reached and there is no
+	 * previous mitigaiton request, then there is nothing to compute.
 	 */
-	if (!throttle)
+	if (!throttle && instance->target == THERMAL_NO_TARGET)
 		return THERMAL_NO_TARGET;
 	/*
 	 * We keep this instance the way it is by default.
